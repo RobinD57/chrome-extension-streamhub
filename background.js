844c36235c28ed81936c172ae0b7a95c654ca70a
RobinD57 <robin.davidseth@gmail.com>
2020-06-09 16:01:30 +0200

    stopped for now

--- background.js
+++ background.js
@@ -3,3 +3,13 @@ chrome.browserAction.onClicked.addListener((tab) => {
     code: 'document.body.style.backgroundColor="#C3413B"'
   });
 });
+
+chrome.cookies.get({ url: 'https://streamhub.watch', name: 'streamhub' },
+  function (cookie) {
+    if (cookie) {
+      console.log(cookie.value);
+    }
+    else {
+      console.log("You aren't logged in on Streamhub.watch!!");
+    }
+});
